74fcc79ecc8f3cfabbbeff64c91a98b8458b3813
ssreenithi <ssreenithi@google.com>
2026-03-03 13:31:46 +0000

    Re-write #mcp-grpc server_version_checks test with parameterization.
    PiperOrigin-RevId: 877895086

--- tests/test_server_version_checks.py
+++ tests/test_server_version_checks.py
@@ -7,33 +7,49 @@ from mcp_grpc_transport.server import grpc_server
 from tests import test_utils
 from mcp_grpc_transport.utils import version_utils
 
-from google.protobuf import struct_pb2
 from google3.testing.pybase import googletest
+from google3.testing.pybase import parameterized
 from mcp_grpc_transport_proto import mcp_messages_pb2
 
 
-class TestRPCVersionChecks(unittest.IsolatedAsyncioTestCase):
+class TestRPCVersionChecks(
+    parameterized.TestCase, unittest.IsolatedAsyncioTestCase
+):
   """Tests the version checking logic of the MCP gRPC server."""
 
   async def asyncSetUp(self):
     self.test_server = test_utils.TestServerWithTools()
     await self.test_server.start_grpc_server()
 
-    self.test_client = test_utils.FakeTestClient(
-        self.test_server.port
-    )
+    self.test_client = test_utils.FakeTestClient(self.test_server.port)
 
   async def asyncTearDown(self):
     await self.test_client.channel.close()
     await grpc_server.stop_grpc_server(self.test_server.grpc_server, 1)
 
-  async def test_list_tools_unsupported_version(self):
-    """Tests the ListTools RPC fails with unsupported version metadata.
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="_ListTools",
+          rpc_name="ListTools",
+          request=mcp_messages_pb2.ListToolsRequest(),
+      ),
+      dict(
+          testcase_name="_CallTool",
+          rpc_name="CallTool",
+          request=mcp_messages_pb2.CallToolRequest(),
+      ),
+  )
+  async def test_rpcs_unsupported_version(self, rpc_name, request):
+    """Tests the different RPCs fail with unsupported version metadata.
 
     The test verifies the following:
     1. The RPC fails with an UNIMPLEMENTED error.
     2. The response metadata is expected to contain the latest protocol version.
     3. The error details are expected to contain the list of supported versions.
+
+    Args:
+        rpc_name: (str) The name of the RPC to test.
+        request: The corresponding Request proto to send to the RPC.
     """
 
     # As of Jan 2025, supported versions are:
@@ -43,10 +59,10 @@ class TestRPCVersionChecks(unittest.IsolatedAsyncioTestCase):
         (version_utils.MCP_PROTOCOL_VERSION_KEY, "2025-06-20"),
     ]
 
-    request = mcp_messages_pb2.ListToolsRequest()
+    rpc = getattr(self.test_client.stub, rpc_name)
 
     with self.assertRaises(grpc.RpcError) as context:
-      await self.test_client.stub.ListTools(
+      await rpc(
           request, metadata=unsupported_version_metadata
       )
 
@@ -65,96 +81,35 @@ class TestRPCVersionChecks(unittest.IsolatedAsyncioTestCase):
         rpc_error.details(),
     )
 
-  async def test_call_tool_unsupported_version(self):
-    """Tests the CallTool RPC fails with unsupported version metadata.
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="_ListTools",
+          rpc_name="ListTools",
+          request=mcp_messages_pb2.ListToolsRequest(),
+      ),
+      dict(
+          testcase_name="_CallTool",
+          rpc_name="CallTool",
+          request=mcp_messages_pb2.CallToolRequest(),
+      ),
+  )
+  async def test_rpcs_missing_version(self, rpc_name, request):
+    """Tests the different RPCs fail with missing version.
 
     The test verifies the following:
     1. The RPC fails with an UNIMPLEMENTED error.
     2. The response metadata is expected to contain the latest protocol version.
     3. The error details are expected to contain the list of supported versions.
+
+    Args:
+        rpc_name: (str) The name of the RPC to test.
+        request: The corresponding Request proto to send to the RPC.
     """
 
-    args = struct_pb2.Struct()
-    args.update({"message": "World"})
-
-    request = mcp_messages_pb2.CallToolRequest(
-        request=mcp_messages_pb2.CallToolRequest.Request(
-            name="echo", arguments=args
-        )
-    )
-
-    # Use a random date to test the unsupported version.
-    unsupported_version_metadata = [
-        (version_utils.MCP_PROTOCOL_VERSION_KEY, "2025-06-20"),
-    ]
+    rpc = getattr(self.test_client.stub, rpc_name)
 
     with self.assertRaises(grpc.RpcError) as context:
-      await self.test_client.stub.CallTool(
-          request, metadata=unsupported_version_metadata
-      )
-
-    rpc_error = context.exception
-    self.assertEqual(rpc_error.code(), grpc.StatusCode.UNIMPLEMENTED)
-
-    metadata = rpc_error.initial_metadata()
-    self.assertEqual(
-        metadata[version_utils.MCP_PROTOCOL_VERSION_KEY],
-        version.LATEST_PROTOCOL_VERSION,
-    )
-
-    supported_versions_str = ", ".join(version.SUPPORTED_PROTOCOL_VERSIONS)
-    self.assertIn(
-        supported_versions_str,
-        rpc_error.details(),
-    )
-
-  async def test_list_tools_missing_version(self):
-    """Tests the ListTools RPC fails with missing version.
-
-    The test verifies the following:
-    1. The RPC fails with an UNIMPLEMENTED error.
-    2. The response metadata is expected to contain the latest protocol version.
-    3. The error details are expected to contain the list of supported versions.
-    """
-    request = mcp_messages_pb2.ListToolsRequest()
-    with self.assertRaises(grpc.RpcError) as context:
-      await self.test_client.stub.ListTools(request)
-
-    rpc_error = context.exception
-    self.assertEqual(rpc_error.code(), grpc.StatusCode.UNIMPLEMENTED)
-
-    metadata = rpc_error.initial_metadata()
-    self.assertEqual(
-        metadata[version_utils.MCP_PROTOCOL_VERSION_KEY],
-        version.LATEST_PROTOCOL_VERSION,
-    )
-
-    supported_versions_str = ", ".join(version.SUPPORTED_PROTOCOL_VERSIONS)
-    self.assertIn(
-        supported_versions_str,
-        rpc_error.details(),
-    )
-
-  async def test_call_tool_missing_version(self):
-    """Tests the CallTool RPC fails with missing version.
-
-    The test verifies the following:
-    1. The RPC fails with an UNIMPLEMENTED error.
-    2. The response metadata is expected to contain the latest protocol version.
-    3. The error details are expected to contain the list of supported versions.
-    """
-
-    args = struct_pb2.Struct()
-    args.update({"message": "World"})
-
-    request = mcp_messages_pb2.CallToolRequest(
-        request=mcp_messages_pb2.CallToolRequest.Request(
-            name="echo", arguments=args
-        )
-    )
-
-    with self.assertRaises(grpc.RpcError) as context:
-      await self.test_client.stub.CallTool(request)
+      await rpc(request)
 
     rpc_error = context.exception
     self.assertEqual(rpc_error.code(), grpc.StatusCode.UNIMPLEMENTED)
